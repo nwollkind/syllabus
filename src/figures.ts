c8e96a9850dd2b2b0b0024d45f0b51126253a230
Olli Saarikivi <olli.saarikivi@iki.fi>
2016-09-04 15:13:45 -0700

    Figures work

--- src/figures.ts
+++ src/figures.ts
@@ -2,36 +2,60 @@
 
 class Figure {
     name: string;
-    timeline: Instant[];
-    constructor(name: string, timeline: Instant[]) {
+    timeline: Step[];
+    constructor(name: string, timeline: Step[]) {
         this.name = name;
         this.timeline = timeline;
     }
 }
 
-interface Instant {
-    time: number;
-    facts: Fact[];
-}
-
-interface Fact {
-
-}
-
 enum Foot { Left, Right }
-enum To { Forward, Backward, Side }
+let LF = Foot.Left;
+let RF = Foot.Right;
 
-class Step implements Fact {
+enum Placement { Backward, Forward, ToSide, Closes }
+let back = Placement.Backward;
+let fwd = Placement.Forward;
+let toSide = Placement.ToSide;
+let closes = Placement.Closes;
+
+enum AlignmentType { Normal, Pointing }
+let pointing = AlignmentType.Pointing
+
+class Step {
+    time: number;
     foot: Foot;
-    to: To;
-    constructor(foot: Foot, to: To) {
+    placement: Placement;
+    alignmentType: AlignmentType;
+    direction: number;
+    constructor(time: number, foot: Foot, placement: Placement, direction: number, alignmentType: AlignmentType = AlignmentType.Normal) {
+        this.time = time;
         this.foot = foot;
-        this.to = to;
+        this.placement = placement;
+        this.alignmentType = alignmentType;
+        this.direction = direction;
     }
 }
 
 export let waltz = [
-    new Figure("Natural turn", [
-        { time: 1, facts: [new Step(Foot.Left, To.Backward)] }
-    ])
+    new Figure("Natural turn forward", [
+        new Step(1, RF, fwd,    0),
+        new Step(2, LF, toSide, 90),
+        new Step(3, RF, closes, 45),
+    ]),
+    new Figure("Natural turn backward", [
+        new Step(1, LF, back,   0),
+        new Step(2, RF, toSide, 135, pointing),
+        new Step(3, LF, closes, 0),
+    ]),
+    new Figure("Reverse turn forward", [
+        new Step(1, LF, fwd,    0),
+        new Step(2, RF, toSide, 90),
+        new Step(3, LF, closes, 45),
+    ]),
+    new Figure("Reverse turn backward", [
+        new Step(1, RF, back,    0),
+        new Step(2, LF, toSide, 135, pointing),
+        new Step(3, RF, closes, 0),
+    ]),
 ];
